39c554d1f06963d06bfcaca2750465ce38bc67f6
SawantAchal <achals1718@gmail.com>
2024-04-13 17:38:32 +0530

    Add sorting function

--- src/components/CityTable.jsx
+++ src/components/CityTable.jsx
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import FavoriteButton from './FavoriteButton';
-import bgHome from '../assets/home2.gif'
+import bgHome from '../assets/home2.gif';
 
 const CityTable = () => {
-  // States to manage cities, countries, time zones, filtered cities, search query, and favorites
   const [cities, setCities] = useState([]);
   const [countries, setCountries] = useState([]);
   const [timeZones, setTimeZones] = useState([]);
   const [filteredCities, setFilteredCities] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [favorites, setFavorites] = useState([]);
+  const [sortOrder, setSortOrder] = useState('ascending');
 
-  // Fetch city data when the component mounts
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -24,12 +23,10 @@ const CityTable = () => {
         const data = await response.json();
         const results = data.results;
 
-        // Extract city names, country names, and time zones from the results
         const cityNames = results.map(result => result.ascii_name);
         const countryNames = results.map(result => result.cou_name_en);
         const timeZoneNames = results.map(result => result.timezone);
 
-        // Set states with fetched data
         setCities(cityNames);
         setCountries(countryNames);
         setTimeZones(timeZoneNames);
@@ -42,7 +39,6 @@ const CityTable = () => {
     fetchData();
   }, []);
 
-  // Function to handle search input change
   const handleSearchChange = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
@@ -57,66 +53,74 @@ const CityTable = () => {
     }
   };
 
-  // Function to handle city click
   const handleCityClick = (e, city) => {
     if (e.button === 0) {
-      // Left click: Navigate to weather page in the same tab
       return;
     } else if (e.button === 1 || e.button === 2) {
-      // Middle click or right click: Open weather page in a new tab
       window.open(`/weather/${city}`, '_blank');
     }
   };
 
-  // Function to handle adding/removing favorites
   const handleFavoriteToggle = (city) => {
     if (favorites.includes(city)) {
       setFavorites(favorites.filter(fav => fav !== city));
-      // Remove city from favorites in local storage
-      localStorage.setItem('favorites', JSON.stringify(favorites.filter(fav => fav !== city)));
     } else {
       setFavorites([...favorites, city]);
-      // Add city to favorites in local storage
-      localStorage.setItem('favorites', JSON.stringify([...favorites, city]));
     }
   };
 
-  // Render the city table component
+  const handleSortOrderChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const sortedCities = () => {
+    const sortableCities = [...filteredCities];
+    sortableCities.sort((a, b) => {
+      if (a.toLowerCase() < b.toLowerCase()) {
+        return sortOrder === 'ascending' ? -1 : 1;
+      }
+      if (a.toLowerCase() > b.toLowerCase()) {
+        return sortOrder === 'ascending' ? 1 : -1;
+      }
+      return 0;
+    });
+    return sortableCities;
+  };
+
   return (
-    <div className="city-table-container bg-cover bg-center bg-fixed" style={{ backgroundImage: `url(${bgHome})` }}>
-      <div className="container mx-auto p-4 bg-white bg-opacity-75 rounded-lg shadow-lg">
+    <div className="city-table-container bg-cover bg-center bg-fixed " style={{ backgroundImage: `url(${bgHome})` }}>
+      <div className="container mx-auto p-4 bg-white bg-opacity-40 rounded-lg shadow-lg">
         <h1 className="text-3xl font-semibold mb-4 text-center">LIST OF CITIES</h1>
-        <div className="flex justify-evenly mb-4">
-          {/* Search input */}
+        <div className="flex justify-between items-center mb-4">
+          <div className="flex items-center space-x-4">
+            <label htmlFor="sortOrder" className="font-bold text-lg">Sort Cities:</label>
+            <select id="sortOrder" value={sortOrder} onChange={handleSortOrderChange} className="border border-black rounded px-2 py-1">
+              <option value="ascending">Ascending</option>
+              <option value="descending">Descending</option>
+            </select>
+          </div>
           <input type='text' placeholder='Search for a city...' value={searchQuery} onChange={handleSearchChange} className="border border-gray-300 rounded px-4 py-2" />
-          {/* Link to favorite locations page */}
-          <Link to="/favorite" className="block ml-4 py-2 px-4 text-blue-500 font-bold rounded border border-blue-500 hover:bg-blue-100 hover:border-blue-700 hover:text-black transition duration-300" style={{ textDecoration: 'none' }}>Favorite Locations</Link>
+          <Link to="/favorite" className="py-2 px-4 font-bold rounded border border-black hover:bg-blue-100  hover:text-black transition duration-300 " style={{ textDecoration: 'none' }}>Favorite Locations</Link>
         </div>
         <div className="overflow-x-auto">
-          {/* Render table of filtered cities */}
           {filteredCities.length > 0 ? (
-            <table className="min-w-full divide-y divide-gray-200 bg-transparent">
-              <thead className="">
+            <table className="min-w-full divide-y divide-black bg-transparent">
+              <thead>
                 <tr>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-bold text-gray-950 uppercase tracking-wider">City Name</th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-bold text-gray-950 uppercase tracking-wider">Country</th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-bold text-gray-950 uppercase tracking-wider">Time Zone</th>
-                  <th scope="col" className="px-6 py-3 text-left text-xs font-bold text-gray-950 uppercase tracking-wider">Favorite</th>
+                  <th scope="col" className="px-6 py-3 text-left text-lg font-bold text-gray-950 uppercase tracking-wider">City Name</th>
+                  <th scope="col" className="px-6 py-3 text-left text-lg font-bold text-gray-950 uppercase tracking-wider">Country</th>
+                  <th scope="col" className="px-6 py-3 text-left text-lg font-bold text-gray-950 uppercase tracking-wider">Time Zone</th>
+                  <th scope="col" className="px-6 py-3 text-left text-lg font-bold text-gray-950 uppercase tracking-wider">Favorite</th>
                 </tr>
               </thead>
               <tbody>
-                {/* Map through filtered cities to render table rows */}
-                {filteredCities.map((city, index) => (
+                {sortedCities().map((city, index) => (
                   <tr key={index}>
-                    {/* City name */}
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <Link to={`weather/${city}`} onClick={(e) => handleCityClick(e, city)} className="text-blue-500 hover:underline">{city}</Link>
+                    <td className="px-6 py-4 whitespace-nowrap uppercase ">
+                      <Link to={`weather/${city}`} onClick={(e) => handleCityClick(e, city)} className="text-black font-bold hover:underline">{city}</Link>
                     </td>
-                    {/* Country */}
                     <td className="px-6 py-4 whitespace-nowrap">{countries[index]}</td>
-                    {/* Time zone */}
                     <td className="px-6 py-4 whitespace-nowrap">{timeZones[index]}</td>
-                    {/* Favorite button */}
                     <td className="px-6 py-4 whitespace-nowrap">
                       <FavoriteButton city={city} isFavorite={favorites.includes(city)} onToggle={handleFavoriteToggle} />
                     </td>
@@ -134,3 +138,16 @@ const CityTable = () => {
 };
 
 export default CityTable;
+
+
+
+
+
+
+
+
+
+
+
+
+
