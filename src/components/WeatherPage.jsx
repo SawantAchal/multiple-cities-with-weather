d4b8938298d660774fde2ece18acd0bd881c60b1
SawantAchal <achals1718@gmail.com>
2024-04-13 15:24:52 +0530

    created a weather page component to fetch and display weather of the city , and conditional background base on weather  condition , render map

---
+++ src/components/WeatherPage.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import L from 'leaflet';
+import sunnyIcon from '../assets/sunny1.jpg';
+import rainyIcon from '../assets/rainyF.jpg';
+import cloudyIcon from '../assets/cloudy1.jpg';
+
+const WeatherPage = () => {
+  // States to manage weather data, unit of measurement, and map
+  const [weatherData, setWeatherData] = useState(null);
+  const [unit, setUnit] = useState('metric');
+  const [map, setMap] = useState(null);
+  // Extract city parameter from URL
+  const { city } = useParams();
+
+  // Fetch weather data when component mounts or city/unit changes
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const apiKey = '62c11a30b625a10b4a038000a78ffdad';
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch weather data');
+        }
+        const data = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchWeatherData();
+  }, [city, unit]);
+
+  // Display map when weather data is available
+  useEffect(() => {
+    if (weatherData) {
+      displayMap();
+    }
+  }, [weatherData]);
+
+  // Disable body overflow when component mounts and re-enable when unmounts
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'visible';
+    };
+  }, []);
+
+  // Function to display map using Leaflet
+  const displayMap = () => {
+    if (!map) {
+      const map = L.map('map').setView([weatherData.coord.lat, weatherData.coord.lon], 10);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+      L.marker([weatherData.coord.lat, weatherData.coord.lon]).addTo(map).bindPopup(city).openPopup();
+      setMap(map);
+    }
+  };
+
+  // Function to handle unit change
+  const handleUnitChange = (event) => {
+    setUnit(event.target.value);
+  };
+
+  // If weather data is not yet fetched, display loading message
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  // Define weather condition icon based on weather data
+  let weatherIcon;
+  if (weatherData.weather[0].main === 'Clear') {
+    weatherIcon = sunnyIcon;
+  } else if (weatherData.weather[0].main === 'Rain') {
+    weatherIcon = rainyIcon;
+  } else if (weatherData.weather[0].main === 'Clouds') {
+    weatherIcon = cloudyIcon;
+  }
+
+  // Inline styles for background image
+  const backgroundImageStyle = {
+    backgroundImage: `url(${weatherIcon})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    padding: '20px',
+    color: 'white',
+  };
+
+  // Render weather information
+  return (
+    <div style={backgroundImageStyle} className="p-8 rounded-lg shadow-xl text-gray-800">
+      <h1 className="text-3xl font-bold mb-4 text-center">Weather for : <span className='font-bold italic text-black underline'>{city}</span></h1>
+      <div>
+        <div className="mb-4">
+          {/* Unit selection dropdown */}
+          <label className="block mb-2 text-lg" htmlFor="unit">Select Unit:</label>
+          <select id="unit" value={unit} onChange={handleUnitChange} className="px-4 py-2 rounded-lg bg-white text-gray-900">
+            <option value="metric">Metric (°C)</option>
+            <option value="imperial">Imperial (°F)</option>
+          </select>
+        </div>
+        {/* Display weather information */}
+        <div className='text-black'>
+          <p className="text-lg">Temperature: <span className="font-bold">{weatherData.main.temp}</span> {unit === 'metric' ? '°C' : '°F'}</p>
+          <p className="text-lg">Weather Description: <span className="font-bold">{weatherData.weather[0].description}</span></p>
+          <p className="text-lg">Humidity: <span className="font-bold">{weatherData.main.humidity}%</span></p>
+          <p className="text-lg">Wind Speed: <span className="font-bold">{weatherData.wind.speed}</span> m/s</p>
+          <p className="text-lg">Atmospheric Pressure: <span className="font-bold">{weatherData.main.pressure}</span> hPa</p>
+          <p className="text-lg">High Temperature: <span className="font-bold">{weatherData.main.temp_max}</span></p>
+          <p className="text-lg">Low Temperature: <span className="font-bold">{weatherData.main.temp_min}</span></p>
+        </div>
+      </div>
+      {/* Render map */}
+      <div id="map" className="mt-8 hidden md:block" style={{ height: '400px' }} ></div>
+    </div>
+  );
+};
+
+export default WeatherPage;
